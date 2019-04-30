7a7e04563b8877fcb3952cd3daa7be8f5fde98d5
sabboud1 <50153031+sabboud1@users.noreply.github.com>
2019-04-30 12:59:12 -0700

    Add files via upload

---
+++ Syl's first Python list exercise.py
@@ -0,0 +1,12 @@
+job_applicants=["John","Geoff","Brady","Thomas"]
+heights=[61,70,67,64,65]
+ints_and_strings=[1,2,3,'four','five']
+empty_list=[]
+print(job_applicants[0])
+print(job_applicants[1])
+
+john_skills=['Python','Communication','LowSalary Requewst',10000]
+print(john_skills)
+applicants=[['John','Python'],['Geoff','Doesnt know python']]
+print(applicants)
+                
