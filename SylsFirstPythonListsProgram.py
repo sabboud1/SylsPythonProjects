d602c2aa31953e6a69c177f38974bb521b594ca1
sabboud1 <50153031+sabboud1@users.noreply.github.com>
2019-04-30 13:14:11 -0700

    Add files via upload

---
+++ SylsFirstPythonListsProgram.py
@@ -0,0 +1,31 @@
+job_applicants=["John","Geoff","Brady","Thomas"]
+heights=[61,70,67,64,65]
+ints_and_strings=[1,2,3,'four','five']
+empty_list=[]
+print(job_applicants[0])
+print(job_applicants[1])
+print()
+
+john_skills=['Python','Communication','LowSalary Requewst',10000]
+print(john_skills)
+applicants=[['John','Python'],['Geoff','Doesnt know python']]
+print(applicants)
+print()
+
+heights=[['Jenny',61],['Alexus',70],['Sam',67],['Grace',64],['Vik',68]]
+ages=[['Aaron',15],['Dhruti',16]]
+print(heights,ages)
+print()
+
+names=['Jenny','Alexus','Samuel','Grace']
+skills=['Python','R','NOTHING','Python']
+names_and_skills=zip(names,skills)
+print(names_and_skills)
+print() 
+
+owners=['Jenny','Alexus','Sam','Grace']
+dogs=['Elphonse','Dr. Doggy DDS','Carter','Ralph']
+owners_and_dogs=zip(owners,dogs)
+print(owners_and_dogs)
+print()
+                                      
