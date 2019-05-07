92e30c4c204d5e9fdf36bcee41f8086341ee81be
sabboud1 <50153031+sabboud1@users.noreply.github.com>
2019-05-07 13:02:01 -0700

    Add files via upload

---
+++ Python lists and looping.py
@@ -0,0 +1,15 @@
+empty_dict={}
+subtotal_to_total={20: 24, 10: 12, 5: 6, 15: 18}
+print(subtotal_to_total)
+person={"name": "Shuri", "age": 18, "siblings": ["TChaka", "Ramonda"]}
+print(person)
+coolness={"forest": 21, "movies": 23, "Tech_Shop": 110, "ice_cream_shop":100}
+print(coolness)
+
+menu={"oatmeal": 3, "avacado toast": 6, "carrot juice": 5, "blueberry muffin": 2}
+menu["cheesecake"]=8
+print(menu)
+
+for item in person:
+    print(person[item])
+
